5840b04a30c04fecb8b3fedc83e6bd5663748d47
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-07-27 14:34:27 +0530

    worked on adding item to list(click event)

--- src/component/List.tsx
+++ src/component/List.tsx
@@ -1,23 +1,37 @@
 import React from 'react'
+import {IState as IProps} from "../App"
+
+// interface IProps {
+//     people: {
+//         name: string,
+//         age: number,
+//         img: string,
+//         note?: string
+//     }[]
+// }
 
 
-interface IProps{
-    people:{
-      name: string,
-      age:number,
-      url:string,
-      note ?: string
-    }[]
-  }
+const List: React.FC<IProps> = ({ people }) => {
 
+    const renderList = (): JSX.Element[] => {
+        return people.map(item => {
+            return (
+                <li className='List' key={item.name}>
+                    <div className='List-header'>
+                        <img src={item.img} alt={item.name} className='List-img' />
+                        <h2 className='List-h2'>{item.name} </h2>
+                    </div>
+                    <p className=''>{item.age} years old</p>
+                </li>
+            )
+        })
+    }
 
-const List = ({people}:IProps) => {
-  return (
-    <div>
-        
-
-    </div>
-  )
+    return (
+        <ul>
+            {renderList()}
+        </ul>
+    )
 }
 
 export default List
